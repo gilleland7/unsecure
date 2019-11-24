1060573f0e4cad27e3df2a6ee41f4773da4e8d95
Matthew Gilleland <gilleland.7@buckeyemail.osu.edu>
2019-11-24 14:18:52 -0500

    restart

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/' => 'users#index'
-post '/users' => 'users#create'
-get '/dashboard' => 'mains#index'
-post '/profiles' => 'profiles#create'
-get 'search', to: "users#search"
-end
